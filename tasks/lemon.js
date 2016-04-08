6741bedc28c2c1a6d84b781600ab4593e6c4cb6f
Raoul v. R <vanruesc@outlook.de>
2016-04-08 18:35:35 +0200

    Version 0.1.0.

--- tasks/lemon.js
+++ tasks/lemon.js
@@ -1,5 +1,5 @@
 /**
- * grunt-lemon v0.0.2 build Apr 05 2016
+ * grunt-lemon v0.1.0 build Apr 08 2016
  * https://github.com/vanruesc/grunt-lemon
  * Copyright 2016 Raoul van Rüschen, Zlib
  */
@@ -9,195 +9,206 @@
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
 var waterfall = _interopDefault(require('async-waterfall'));
-var glob = _interopDefault(require('glob'));
 var path = _interopDefault(require('path'));
 var fs = _interopDefault(require('fs'));
+var glob = _interopDefault(require('glob'));
 
-function lemon(grunt) {
+/**
+ * Inlines file imports.
+ *
+ * @param {String} file - The file.
+ * @param {Object} options - The options.
+ * @param {String} options.encoding - The encoding of the given file.
+ * @param {Object} options.extensions - The import file extensions to consider. Each extension must define an encoding.
+ * @param {Boolean} options.useVar - Whether the var declaration should be used instead of const.
+ * @param {Function} done - A callback function.
+ */
 
-	grunt.registerMultiTask("lemon", "Inline text file imports.", function() {
+function squeeze(file, options, done) {
+
+	const declaration = options.useVar ? "var" : "const";
+
+	// Ignoring the (optional) semicolon.
+	const importRegExp = /import\s*(\w*)\s*from\s*[\"\'](.*)[\"\']/ig;
+
+	waterfall([
+
+		function checkFile(next) {
+
+			fs.access(file, (fs.R_OK | fs.W_OK), next);
+
+		},
+
+		function readFile(next) {
+
+			fs.readFile(file, options.encoding, next);
+
+		},
+
+		function parseImports(data, next) {
+
+			const imports = [];
+
+			let result = importRegExp.exec(data);
+
+			while(result !== null) {
+
+				imports.push({
+					start: result.index,
+					end: importRegExp.lastIndex,
+					name: result[1],
+					path: path.resolve(path.dirname(file), result[2]),
+					encoding: options.extensions[path.extname(result[2])],
+					data: null
+				});
+
+				result = importRegExp.exec(data);
+
+			}
+
+			// Might have no imports at all.
+			next(null, imports, data);
+
+		},
+
+		function filterImports(imports, data, next) {
+
+			const filteredImports = [];
+
+			let i, l;
+
+			for(i = 0, l = imports.length; i < l; ++i) {
+
+				if(imports[i].encoding !== undefined) {
+
+					filteredImports.push(imports[i]);
+
+				}
+
+			}
+
+			// Might end up with no imports.
+			next(null, filteredImports, data);
+
+		},
+
+		function checkImports(imports, data, next) {
+
+			let i = 0;
+			let l = imports.length;
+
+			(function proceed(error) {
+
+				if(error || i === l) {
+
+					next(error, imports, data);
+
+				} else {
+
+					fs.access(imports[i++].path, (fs.R_OK | fs.W_OK), proceed);
+
+				}
+
+			}());
+
+		},
+
+		function readImports(imports, data, next) {
+
+			let j;
+			let i = -1;
+			let l = imports.length;
+
+			(function proceed(error, importData) {
+
+				j = i;
+
+				if(error || ++i === l) {
+
+					// Check if there are any imports.
+					if(l > 0) {
+
+						// If so, don't forget to pick up the one that was read last.
+						imports[j].data = importData;
+
+					}
+
+					next(error, imports, data);
+
+				} else {
+
+					// Skip this during the first run.
+					if(i > 0) {
+
+						// Collect the data. The index i is one step ahead of j.
+						imports[j].data = importData;
+
+					}
+
+					fs.readFile(imports[i].path, imports[i].encoding, proceed);
+
+				}
+
+			}());
+
+		},
+
+		function inlineImports(imports, data, next) {
+
+			let modified = imports.length > 0;
+			let i;
+
+			// Inline the imports in reverse order to keep the indices intact.
+			while(imports.length > 0) {
+
+				i = imports.pop();
+
+				data = data.substring(0, i.start) +
+					declaration + " " + i.name + " = " + JSON.stringify(i.data) +
+					data.substring(i.end);
+
+			}
+
+			next(null, modified, data);
+
+		},
+
+		function writeFile(modified, data, next) {
+
+			if(modified) {
+
+				fs.writeFile(file, data, next);
+
+			} else {
+
+				next(null);
+
+			}
+
+		}
+
+	], done);
+
+}
+
+/**
+ * The lemon grunt task.
+ *
+ * @param {Object} grunt - The grunt instance.
+ */
+
+function task(grunt) {
+
+	grunt.registerMultiTask("lemon", "Inline file imports.", function() {
 
 		const options = this.options({
-			extensions: [],
+			extensions: {},
 			encoding: "utf8",
 			useVar: false,
 			glob: null
 		});
 
-		const done = this.async();
 		const src = this.data.src;
-
-		const declaration = options.useVar ? "var" : "const";
-
-		// Ignoring the (optional) semicolon on purpose.
-		const importRegExp = /import\s*(\w*)\s*from\s*[\"\'](.*)[\"\']/ig;
-
-		/**
-		 * Inlines text imports in the given file.
-		 *
-		 * @param {String} file - The file.
-		 * @param {Function} finish - A callback function.
-		 */
-
-		function squeeze(file, finish) {
-
-			waterfall([
-
-				function checkFile(next) {
-
-					fs.access(file, (fs.R_OK | fs.W_OK), next);
-
-				},
-
-				function readFile(next) {
-
-					fs.readFile(file, options.encoding, next);
-
-				},
-
-				function parseImports(data, next) {
-
-					const imports = [];
-
-					let result = importRegExp.exec(data);
-
-					while(result !== null) {
-
-						imports.push({
-							start: result.index,
-							end: importRegExp.lastIndex,
-							name: result[1],
-							path: path.resolve(path.dirname(file), result[2]),
-							data: null
-						});
-
-						result = importRegExp.exec(data);
-
-					}
-
-					// Might end up with no imports.
-					next(null, imports, data);
-
-				},
-
-				function filterImports(imports, data, next) {
-
-					const filteredImports = [];
-
-					let i, l;
-
-					for(i = 0, l = imports.length; i < l; ++i) {
-
-						if(options.extensions.indexOf(path.extname(imports[i].path)) > -1) {
-
-							filteredImports.push(imports[i]);
-
-						}
-
-					}
-
-					// Might end up with no imports.
-					next(null, filteredImports, data);
-
-				},
-
-				function checkImports(imports, data, next) {
-
-					let i = 0;
-					let l = imports.length;
-
-					(function proceed(error) {
-
-						if(error || i === l) {
-
-							next(error, imports, data);
-
-						} else {
-
-							fs.access(imports[i++].path, (fs.R_OK | fs.W_OK), proceed);
-
-						}
-
-					}());
-
-				},
-
-				function readImports(imports, data, next) {
-
-					let i = 0;
-					let l = imports.length;
-
-					(function proceed(error, importData) {
-
-						if(error || i === l) {
-
-							// Check if there are any imports.
-							if(l > 0) {
-
-								// If so, don't forget to pick up the one that was read last.
-								imports[(i - 1)].data = importData;
-
-							}
-
-							next(error, imports, data);
-
-						} else {
-
-							// Skip this during the first run.
-							if(i > 0) {
-
-								// Collect the data. The index is one step ahead.
-								imports[(i - 1)].data = importData;
-
-							}
-
-							fs.readFile(imports[i++].path, options.encoding, proceed);
-
-						}
-
-					}());
-
-				},
-
-				function inlineImports(imports, data, next) {
-
-					let modified = imports.length > 0;
-					let i;
-
-					// Inline the imports in reverse order to keep the indices intact.
-					while(imports.length > 0) {
-
-						i = imports.pop();
-
-						data = data.substring(0, i.start) +
-							declaration + " " + i.name + " = " + JSON.stringify(i.data) +
-							data.substring(i.end);
-
-					}
-
-					next(null, modified, data);
-
-				},
-
-				function writeFile(modified, data, next) {
-
-					if(modified) {
-
-						fs.writeFile(file, data, next);
-
-					} else {
-
-						next(null);
-
-					}
-
-				}
-
-			], finish);
-
-		}
-
-		// Main process.
 
 		waterfall([
 
@@ -230,7 +241,7 @@ function lemon(grunt) {
 
 					} else {
 
-						squeeze(files[i++], proceed);
+						squeeze(files[i++], options, proceed);
 
 					}
 
@@ -238,10 +249,10 @@ function lemon(grunt) {
 
 			}
 
-		], done);
+		], this.async());
 
 	});
 
 }
 
-module.exports = lemon;
+module.exports = task;
